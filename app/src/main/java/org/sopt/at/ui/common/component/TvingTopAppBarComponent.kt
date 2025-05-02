23c057bbceda65920612dfbd01dcd165d82396db
Daljyeong <jiyoungm1022@gmail.com>
2025-05-03 00:05:48 +0900

    [fix/#5] png를 imageVector로 받아서 발생했던 오류 수정

--- app/src/main/java/org/sopt/at/ui/common/component/TvingTopAppBarComponent.kt
+++ app/src/main/java/org/sopt/at/ui/common/component/TvingTopAppBarComponent.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,7 +60,7 @@ fun TvingTopAppBarComponent(
         // 우측 아이콘 2
         if (rightSecondIconId != null) {
             Icon(
-                imageVector = ImageVector.vectorResource(rightSecondIconId),
+                painter = painterResource(rightSecondIconId),
                 contentDescription = "TopAppBar rightSecondIconId",
                 tint = Color.Unspecified,
                 modifier = Modifier
